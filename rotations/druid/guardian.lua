548618964b2899df23ec13906349ccfad5acd690
Rabalaba <nicholaszimniski@Nicholass-Mac-Pro.local>
2016-11-19 01:46:16 -0800

    fixed guardian

--- rotations/druid/guardian.lua
+++ rotations/druid/guardian.lua
@@ -2,6 +2,12 @@ local _, Rabbs = ...
 
 local exeOnLoad = function()
 	Rabbs.ExeOnLoad()
+    NeP.Interface:AddToggle({
+		key = 'taunt',
+		name = 'Taunt Units',
+		text = 'Enable to taunt units you dont have aggro from.',
+		icon = 'Interface\\Icons\\Ability_warrior_charge',
+	})
 
 	print('|cffADFF2F ----------------------------------------------------------------------|r')
 	print('|cffADFF2F --- |rDRUID |cffFFA500Guardian |r')
@@ -25,21 +31,22 @@ local Interrupts = {
 }
 
 local Cooldowns = {
-	{'Frenzied Regeneration', '{player.health<=75&new_incdmg>{0.3*player.health.max}&!player.buff(Frenzied Regeneration)}||{player.health<=25&new_incdmg>{0.15*player.health.max}&!player.buff(Frenzied Regeneration)}'},
-    {'Rage of the Sleeper', '{player.health<=60&new_incdmg>{0.4*player.health.max}&!player.buff(Frenzied Regeneration)}||{player.health<=20&new_incdmg>{0.2*player.health.max}&!player.buff(Frenzied Regeneration)}'},
+	{'Frenzied Regeneration', '{player.health<=75&new_incdmg>{player.health.max}&!player.buff(Frenzied Regeneration)}||{player.health<=25&new_incdmg>{0.15*player.health.max}&!player.buff(Frenzied Regeneration)}'},
+    {'Rage of the Sleeper', '{player.health<=60&new_incdmg>{player.health.max}&!player.buff(Frenzied Regeneration)}||{player.health<=20&new_incdmg>{0.2*player.health.max}&!player.buff(Frenzied Regeneration)}'},
     {'Ironfur', 'incdmg(5).phys>incdmg(5).magic&player.rage>90'},
     {'Mark of Ursol', 'incdmg(5).magic>incdmg(5).phys&player.rage>90'},
 	{'Ironfur', 'incdmg(5).phys>incdmg(5).magic&{!player.buff(Ironfur)&{player.buff(Guardian of Elune)||player.rage>65}}'},
 	{'Mark of Ursol', 'incdmg(5).magic>incdmg(5).phys&{!player.buff(Mark of Ursol)&{player.buff(Guardian of Elune)||player.rage>65}}'},
     {'Ironfur', 'incdmg(5).phys>incdmg(5)&new_incdmg>{0.3*player.health.max}&player.rage>45'},
     {'Mark of Ursol', 'incdmg(5).magic>incdmg(5).phys&new_incdmg>{0.3*player.health.max}&player.rage>45'},
-    {'Barkskin', 'new_incdmg>{0.25*player.health}&!player.buff(Ironfur)&!player.buff(Mark of Ursol)'},
-    {'Survival Instincts', '!lastgcd(Barkskin)&new_incdmg>{0.75*player.health}&!player.buff(Barkskin)&!player.buff(Ironfur)&!player.buff(Mark of Ursol)'},
-	{'Bristling Fur', '{new_incdmg/player.health.max}>{100-player.rage}&player.rage<45'},
+    {'Barkskin', 'new_incdmg>{0.75*player.health.max}&!player.buff(Ironfur)&!player.buff(Mark of Ursol)'},
+    {'Survival Instincts', '!lastgcd(Barkskin)&new_incdmg>{player.health.max}&!player.buff(Barkskin)&!player.buff(Ironfur)&!player.buff(Mark of Ursol)'},
+	{'Bristling Fur', '{100*new_incdmg/player.health.max}>{100-player.rage}&player.rage<45'},
 	{'Incarnation: Guardian of Ursoc', 'target.ttd > 30'}
 }
 
 local AOE = {
+    {'Mangle', nil,'target'},
 	{'Thrash', nil, 'target'},
 	{'Swipe', nil, 'target'},
 }
@@ -65,4 +72,4 @@ local outCombat = {
 	{Keybinds},
 }
 
-NeP.CR:Add(104, '[|cff'..Rabbs.addonColor..'Rabbs|r] DRUID - Feral', inCombat, outCombat,exeOnLoad)
+NeP.CR:Add(104, '[|cff'..Rabbs.addonColor..'Rabbs|r] DRUID - Guardian', inCombat, outCombat,exeOnLoad)
